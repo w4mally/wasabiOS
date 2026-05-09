7a343c221c2edf9d2f0e05a3cb01789e99827781
w4mally <kazuma246r@gmail.com>
2026-05-09 15:42:37 +0900

    writeln!を使えるようにした

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use core::panic::PanicInfo;
 use core::ptr::null_mut;
 // use core::slice;
 use core::cmp::min;
+use core::fmt;
+use core::fmt::Write;
+use core::writeln;
 
 type EfiVoid = u8;
 type EfiHandle = u64;
@@ -139,6 +142,10 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         draw_font_fg(&mut vram, i as i64*16+256, i as i64*16, 0xffffff, c);
     }
     draw_str_fg(&mut vram, 256, 256, 0xffffff, "Hello, world!");
+    let mut w = VramTextWriter::new(&mut vram);
+    for i in 0..4{
+        writeln!(w, "i={i}").unwrap();
+    }
     loop {
         hlt()
     }
@@ -349,5 +356,33 @@ fn draw_font_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, c: char) {
 fn draw_str_fg<T: Bitmap>(buf: &mut T, x: i64, y: i64, color: u32, s: &str){
     for(i, c) in s.chars().enumerate(){
         draw_font_fg(buf, x + i as i64 * 8, y, color, c);
+    }
+}
+
+/*
+impl: 型に対する関数の実装
+ */
+struct VramTextWriter<'a>{
+    vram: &'a mut VramBufferInfo,
+    cursor_x: i64,
+    cursor_y: i64,
+}
+impl<'a> VramTextWriter<'a>{
+    fn new(vram: &'a mut VramBufferInfo) -> Self {
+        Self { vram, cursor_x: 0, cursor_y: 0 }
+    }
+}
+impl fmt::Write for VramTextWriter<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars(){
+            if c == '\n' {
+                self.cursor_y += 16;
+                self.cursor_x = 0;
+                continue;
+            }
+            draw_font_fg(self.vram, self.cursor_x, self.cursor_y, 0xffffff, c);
+            self.cursor_x += 8;
+        }
+        Ok(())
     }
 }
